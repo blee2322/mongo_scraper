5d209098521ee3f540bfc2656a541d92df2bbdd9
unknown <brandon.geeman.lee@gmail.com>
2017-11-14 19:26:17 -0800

    scraper is working

--- server.js
+++ server.js
@@ -1,17 +1,125 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var request = require('request');
 var cheerio = require('cheerio');
+var axios = require('axios');
+var exphbs = require("express-handlebars");
 var path = require('path');
+var logger = require('morgan');
 
-var Article = require('./models/articles.js');
-var Comment = require('./models/');
+var db = require('./models')
+
+var PORT = 3000;
 
 var app= express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: false}));
 
+//Handlebars====================================================
 var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  partialsDir: path.join(__dirname,'/views/layouts/partials')
+}));
+app.set('view engine', 'handlebars');
+//==============================================================
 
-app.engine('handlebars', exphbs({defaultLayout}))
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static(__dirname + "/public"));
+
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/NYT', {
+  useMongoClient:true
+});
+
+//======Routes====================
+app.get('/scrape', function(req, res) {
+  axios.get('http://www.nytimes.com').then(function(response){
+
+    var $ = cheerio.load(response.data);
+
+    $('article.theme-summary').each(function(i, element) {
+
+      var result = {};
+
+      result.title = $(this)
+        .children('h2.story-heading')
+        .text();
+      result.link = $(this)
+        .children('h2.story-heading')
+        .children('a')
+        .attr('href');
+      result.summary = $(this)
+        .children('.summary')
+        .text();
+
+      db.Article
+        .create(result)
+        .then(function(dbArticle) {
+          res.send('Scrape Complete');
+        })
+        .catch(function(err) {
+          res.json(err);
+        });
+    });
+  });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article
+    .find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article
+    .findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note
+    .create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
