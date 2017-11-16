32a967e6b40434f41e475ada9b4b0613573a3d53
unknown <brandon.geeman.lee@gmail.com>
2017-11-16 15:45:17 -0800

    modal is working

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -31,4 +31,32 @@ $(document).ready(function() {
         window.location = 'saved'
       })
   })
+
+  $('.saveNote').on('click', function() {
+    var thisId = $(this).attr('data-id');
+      $.ajax({
+        method: 'POST',
+        url: 'notes/save/' + thisId,
+        data: {
+          text: $('#noteText' + thisId).val()
+        }
+      }).done(function(data) {
+        console.log(data);
+        // $('#noteText' + thisId).val('');
+        $('.modalNote').modal('hide');
+        window.location ='/saved'
+      })
+  });
+
+  $('.deleteNote').on('click', function(){
+    var noteId = $(this).attr('data-note-id');
+    var articleId = $(this).attr('data-article-id');
+    $.ajax({
+        method: 'DELETE',
+        url: '/notes/delete/' + noteId + '/' + articleId
+    }).done(function(data) {
+      $('.modalNote').modal('hide');
+      window.location = '/saved'
+    })
+  })
 })
